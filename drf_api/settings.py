f704e00763b8ab49eaa11a79cabf3a57bbfa16f1
Judit3 <katapunpinpun@yahoo.es>
2024-03-30 11:44:57 +0000

    add class Profile into models of profiles

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-if os.path.exist('env.py'):
+if os.path.exists('env.py'):
     import env
 
 CLOUDINARY_STORAGE = {
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+
+    'profiles',
 ]
 
 MIDDLEWARE = [
